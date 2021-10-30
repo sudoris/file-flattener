91c3de8bc793d40025afb50254b2e617f845edea
David Chen <zdent09@gmail.com>
2021-10-31 02:21:40 +0800

    opendir using path given in command line argument

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-const fs = require('fs')
+import * as fs from 'fs/promises'
+import { argv } from 'process'
+import * as path from 'path'
 
+// print process.argv
+// argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+
+if (!argv[2]) {
+  console.log("directory not found")
+  process.exit()
+}
+
+const targetDir = argv[2]
+
+try {
+  const dir = await fs.opendir(targetDir);
+  for await (const dirent of dir) {
+    if (dirent.isDirectory()) {
+      console.log('dir name: ', dirent.name);
+    } else {
+      console.log('file name: ', dirent.name)
+    }
+  }
+} catch (err) {
+  console.error(err);
+}
