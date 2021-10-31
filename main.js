74e96100f49e5f3d9f47052322c304679684252f
David Chen <zdent09@gmail.com>
2021-11-01 02:22:43 +0800

    pass file paths to moveFile function

--- main.js
+++ main.js
@@ -2,27 +2,46 @@ import * as fs from 'fs/promises'
 import { argv } from 'process'
 import * as path from 'path'
 
-// print process.argv
+// Print process.argv
 // argv.forEach((val, index) => {
 //   console.log(`${index}: ${val}`);
 // });
 
-if (!argv[2]) {
-  console.log("directory not found")
+// if (!argv[2]) {
+//   console.log("directory not found")
+//   process.exit()
+// }
+
+const targetDir = argv[2]
+let rootDir
+
+// Attempt to open target directory
+try {
+  rootDir = await fs.opendir(targetDir); 
+} catch (err) {
+  console.error(err);
   process.exit()
 }
 
-const targetDir = argv[2]
-
-try {
-  const dir = await fs.opendir(targetDir);
-  for await (const dirent of dir) {
-    if (dirent.isDirectory()) {
-      console.log('dir name: ', dirent.name);
-    } else {
-      console.log('file name: ', dirent.name)
-    }
-  }
-} catch (err) {
-  console.error(err);
+function moveFile(currentPath, targetPath) {
+  console.log('target path: ', targetPath)
 }
+
+for await (const dirent of rootDir) {
+  if (dirent.isDirectory()) {
+    const currentDir = path.resolve(targetDir, dirent.name)
+    const files = await fs.readdir(currentDir);
+    for (const name of files) {
+      if (name.endsWith('.mp4')) {
+        const currentPath = path.resolve(currentDir, name)
+        const targetPath = path.resolve(targetDir, name)
+        moveFile(currentPath, targetPath)
+      }
+    }
+  } 
+}
+
+
+
+
+
